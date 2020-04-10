a8e2967f41af351f2bc47c04de0b922344a2ccb0
TushitNag11 <kanurit@student.douglascollege.ca>
2020-04-09 20:57:00 -0700

    created receipt page and final changes

--- js/seats.js
+++ js/seats.js
@@ -4,13 +4,19 @@ $(document).ready(function(){
 
   $("#continue").on('click',function(){
 
-    var numOfSeats = localStorage.getItem("Seats");
+    var numOfSeats = sessionStorage.getItem("Seats");
     var seatSelected = $('.seat.active').length;
 
+    if(seatSelected!=numOfSeats)
+    {
+      alert("Please select correct number of seats");
+    }
+    else{
       sessionStorage.setItem("seat Num",seatNo);
-    var price = numOfSeats * 10;
+      var price = numOfSeats * 10;
       sessionStorage.setItem("price",price);
-
+      document.getElementById("book").action = "/Receipt.html";
+    }
   });
 $('.cinema-seats .seat').on('click', function() {
     $(this).toggleClass('active');
